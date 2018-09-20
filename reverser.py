0793dd1451c0b183c6ca1a5bcfe4f4c40ba71bf7
hmalle <hmalle90ak@gmail.com>
2018-09-20 10:42:19 -0700

    A working version

--- reverser.py
+++ reverser.py
@@ -3,7 +3,7 @@
 # This enforces active engagement thats very important in learning new skills
 #
 
-import pygame, sys, string, random
+import pygame, sys, string, random, time
 
 #Colors
 blue =(0,128,255)
@@ -12,6 +12,8 @@ white=(255,255,255)
 red  =(255,0,0)
 green=(255,0,0)
 color1=(56,104,106)
+orange=(255,165,0)
+tomato_red=(255,60,0)
 
 #Variables
 width =800
@@ -25,17 +27,20 @@ screen=pygame.display.set_mode((width, height))
 pygame.display.set_caption("reverser")
 clock=pygame.time.Clock()
 
-def check_answer(wordlist[], user_word):
-    for word in wordlist:
-        if user_word==word:
-           wordlist.remove(word) 
-
-class Word(pygame.sprite.Sprite):
+class Word():
     def __init__(self, level):
-        pygame.sprite.Sprite.__init__(self)
-        self.x=random.randint(0,width)
+        self.x=random.randint(0,width-10) #TODO: Minus word length box
         self.y=0
         self.text=self.generate_random_word(level)
+        self.color=self.get_color()
+
+    def get_color(self):
+        if self.y>(height-100):
+            return red
+        elif self.y>(height-200):
+            return orange
+        else:
+            return white
 
     def is_past_height(self):
         if(self.y>height):
@@ -43,38 +48,65 @@ class Word(pygame.sprite.Sprite):
         else: 
             return False
 
-    def update_position(self):
-        self.y+=5 
+    def update_word(self):
+        self.y+=2  #Move the word 1 pixel down
+        self.color=self.get_color()
 
     def generate_random_word(self, char_count):
-        return "".join(random.choice(string.letters+string.digits)for x in range(char_count))
+        random_word="".join(random.choice(string.letters+string.digits)for x in range(char_count))
+        return random_word.lower()
     
 def main():
     #The game loop
     done=False
-    player = Player()
     char_count=5
-    word=Word(char_count)
     wordlist=[]
+    wordlist.append(Word(char_count))
+    time_interval=4
+    correct=0
+    missed_words=0
     user_word=""
+    start=time.time()
     while not done:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+            #When the enter key is pressed, check the word thats been entered
             if event.type==pygame.KEYDOWN:
-                if event.key == pygame.K_ENTER:
-                    check_answer(user_word, wordlist)
- 
-        #Generate random words that drops on the screen
+                if event.key==pygame.K_RETURN:
+                    print(user_word)
+                    for word in wordlist:
+                        if word.text==user_word:
+                            wordlist.remove(word)
+                            correct+=1
+                    user_word=""
+                #Check the lower case asciis and the digits only
+                elif (event.key>=97 and event.key<=122) or (event.key>=48 and event.key<=57):
+                    user_word+=str(unichr(event.key))
+                else:
+                    pass
+
         screen.fill(black)
+        #Generate a new word after every few seconds
+        now=time.time();
+        if now-start>=time_interval:
+            start=time.time()
+            wordlist.append(Word(char_count))
+
+        #Render words on the screen and delete those not completeled
         for word in wordlist:
-            
-        text=font.render(word.text, True, white)
-        screen.blit(text,(word.x,word.y))
+            if word.y > height:
+                wordlist.remove(word)
+                missed_words+=1
+            else:
+                text=font.render(word.text, True, word.color)
+                screen.blit(text,(word.x,word.y))
+                word.update_word()  #Update here to avoid having anther loop
+
         pygame.display.update()
-        word.update_position()
-        clock.tick(20)      #Frames per second
+
+        clock.tick(20) #Frames per second
 
 if __name__ == '__main__':
     main()
