a97598d0c0dd3874269e942b5f1277720c48e380
hmalle <hmalle90ak@gmail.com>
2018-10-19 07:20:11 -0700

    words being generated properly in good time intervals

--- reverser.py
+++ reverser.py
@@ -6,29 +6,45 @@ This enforces active engagement thats very important in learning new skills
 
 import pygame, sys
 from math import log
+import time
 import random, string
 
 class Word:
-    def __init__(self, level, width,height):
+    def __init__(self, level, width, height):
         self.level= level
-        self.text=generate(self.level)
+        self.text=self.generate()
         self.x=0
         self.y=0
-        self.text_h=0 
-        self.text_h=0
         self.screen_w=width
         self.screen_h=height
         self.level=level
 
+    def generate(self):
+        length=5+int(log(self.level))
+        text=""
+        for _ in range(length):
+            text+=random.choice(string.letters+string.digits)
+        print('word generated '+text)
+        return text
+        
 class Player:
     def __init__(self, screen_w, screen_h):
         self.screen_w = screen_w
         self.screen_h = screen_h
-        self.words_list=[]
+        self.words=[]
         self.level=1
+        self.interval=5 #5 seconds between words generation
+        self.last_time=time.time()
+
+    def update(self):
+        time_now = time.time()
+        if time_now-self.last_time >= self.interval:
+            self.last_time=time_now
+            self.add_new_word()
+
 
     def add_new_word(self):
-        self.wordlist.append(Word(self.level,screen_w, screen_h))
+        self.words.append(Word(self.level,self.screen_w, self.screen_h))
 
 def main():
     #data
@@ -41,6 +57,9 @@ def main():
     font=pygame.font.SysFont('monospace',30)
     pygame.display.set_caption("revereserever")
     clock=pygame.time.Clock()
+    
+    #Our character
+    player = Player(width,height)
 
     while(True):
         for event in pygame.event.get():
@@ -48,7 +67,11 @@ def main():
                 pygame.font.quit()
                 pygame.quit()
                 sys.exit()
-    
+
+        player.update()
+
+        clock.tick(fps)
+
     pygame.quit()
 
 if __name__=='__main__': main()
